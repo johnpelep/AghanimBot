d0f37a95d8842c3d41db3433e4aa0ceb9c162f6b
John Philip Cesar P. Morales <johnphilipcesarmorales@gmail.com>
2021-05-16 13:54:07 +0800

    fixed hindaw for name with multiple space

--- commands/hindaw.js
+++ commands/hindaw.js
@@ -7,41 +7,49 @@ module.exports = {
   name: 'hindaw',
   description: 'Hindaw!',
   async execute(message, args) {
-   
-
     // check if there is argument
-    if (!args.length) 
-      return message.reply('no player name specified');
+    if (!args) return message.reply('no player name specified');
 
-    const personaName = args.join(' ');
+    const personaName = args;
 
     // get account from db
-    let account = await accountService.getAccount({ personaName: personaName });
+    let account = await accountService.getAccount({ personaName: args });
 
     // check if account exist
-    if (!account) 
-      return message.reply(`account **${personaName}** is wara sa listahan. Paki-add anay gamit an **Invite!** command`);
+    if (!account)
+      return message.reply(
+        `account **${personaName}** is wara sa listahan. Paki-add anay gamit an **Invite!** command`
+      );
 
     // sync account
     account = await accountHelper.syncAccount(account);
 
     // check if account has record
     if (!account.record || !account.record.streakCount)
-      return message.reply(`account **${personaName}** has no match recorded for this month`);
-    
+      return message.reply(
+        `account **${personaName}** has no match recorded for this month`
+      );
+
     const messageObj = createEmbeddedMessage(account);
-    
+
     return message.channel.send(messageObj);
-  }
-}
+  },
+};
 
 function createEmbeddedMessage(account) {
   const record = account.record;
-  const color = record.streakCount > 1 && record.isWinStreak ? 0x89ff89 : record.streakCount > 1 && !record.isWinStreak ? 0xff4534 : 0x0099ff; 
-  
-  let winRate = (record.winCount * 100 / (record.winCount + record.lossCount)).toFixed(2) + '%';
-  if (winRate.endsWith('.00%'))
-    winRate = winRate.slice(0, -4) + '%';
+  const color =
+    record.streakCount > 1 && record.isWinStreak
+      ? 0x89ff89
+      : record.streakCount > 1 && !record.isWinStreak
+      ? 0xff4534
+      : 0x0099ff;
+
+  let winRate =
+    ((record.winCount * 100) / (record.winCount + record.lossCount)).toFixed(
+      2
+    ) + '%';
+  if (winRate.endsWith('.00%')) winRate = winRate.slice(0, -4) + '%';
 
   const embedMessage = {
     color: color,
@@ -50,38 +58,38 @@ function createEmbeddedMessage(account) {
       {
         name: '\u200b',
         value: '\u200b',
-        inline: false
+        inline: false,
       },
       {
         name: 'Total Games',
         value: record.winCount + record.lossCount,
-        inline: false
+        inline: false,
       },
       {
         name: 'Wins',
         value: record.winCount,
-        inline: false
+        inline: false,
       },
       {
         name: 'Losses',
         value: record.lossCount,
-        inline: false
+        inline: false,
       },
       {
         name: 'Win Rate',
         value: winRate,
-        inline: false
-      }
-    ]
-  };
-
-  if (record.streakCount > 1) {
-    embedMessage.fields.push({
+        inline: false,
+      },
+      {
         name: 'Streak',
-        value: `${record.streakCount} ${record.isWinStreak ? 'wins' : 'losses'}`,
-        inline: false
-    });
-  }
+        value:
+          record.streakCount == 1
+            ? 'No streak'
+            : `${record.streakCount} ${record.isWinStreak ? 'wins' : 'losses'}`,
+        inline: false,
+      },
+    ],
+  };
 
   return { embed: embedMessage };
 }
--- index.js
+++ index.js
@@ -5,42 +5,46 @@ const { token } = require('./config');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter((file) => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
 }
 
 client.once('ready', () => {
-	console.log('Ready!');
+  console.log('Ready!');
 });
 
-client.on('message', async message => {
-	if (message.author.bot) return;
+client.on('message', async (message) => {
+  if (message.author.bot) return;
 
-	const args = message.content.trim().split(/ +/);
-	const commandName = args.shift().toLowerCase().slice(0, -1); //remove !
+  let args = message.content.trim().split(/ +/);
+  const commandName = args.shift().toLowerCase().slice(0, -1); //remove !
 
-	if (!client.commands.has(commandName)) return;
+  if (!client.commands.has(commandName)) return;
 
-	const command = client.commands.get(commandName);
+  const command = client.commands.get(commandName);
 
-	try {
-+		await command.execute(message, args);
-	} catch (error) {
-		console.error(error);
-		
-		const embedMessage = {
-			color: 0x0099ff,
-			title: 'Noooooo! Oh well',
-			footer: {
-				text: 'nagka-error hehe. try nala utro hehehe'
-			}
-		}
-		
-		return message.reply({ embed: embedMessage });
-	}
+  try {
+    if (commandName == 'hindaw')
+      args = message.content.replace('hindaw!', '').trim();
+    await command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+
+    const embedMessage = {
+      color: 0x0099ff,
+      title: 'Noooooo! Oh well',
+      footer: {
+        text: 'nagka-error hehe. try nala utro hehehe',
+      },
+    };
+
+    return message.reply({ embed: embedMessage });
+  }
 });
 
 client.login(token);
