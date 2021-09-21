8bbe1709146172d6bcac8e43677648d50b9050e6
John Philip Cesar P. Morales <johnphilipcesarmorales@gmail.com>
2021-09-21 23:14:14 +0800

    hindaw - removed winrate computation

--- commands/hindaw.js
+++ commands/hindaw.js
@@ -42,20 +42,13 @@ function createEmbeddedMessage(account) {
     (r) =>
       r.month == dateInPh.getMonth() + 1 && r.year == dateInPh.getFullYear()
   )[0];
-  const noRecord = !record || !record.streakCount;
+  const hasRecord = record && record.streakCount;
   const color =
     record.streakCount > 1 && record.isWinStreak
       ? 0x89ff89
       : record.streakCount > 1 && !record.isWinStreak
       ? 0xff4534
       : 0x0099ff;
-
-  let winRate = !noRecord
-    ? ((record.winCount * 100) / (record.winCount + record.lossCount)).toFixed(
-        2
-      ) + '%'
-    : '-';
-  if (winRate.endsWith('.00%')) winRate = winRate.slice(0, -4) + '%';
 
   const embedMessage = {
     color: color,
@@ -74,27 +67,27 @@ function createEmbeddedMessage(account) {
       },
       {
         name: 'Total Games',
-        value: !noRecord ? record.winCount + record.lossCount : '-',
+        value: hasRecord ? record.winCount + record.lossCount : '-',
         inline: false,
       },
       {
         name: 'Wins',
-        value: !noRecord ? record.winCount : '-',
+        value: hasRecord ? record.winCount : '-',
         inline: false,
       },
       {
         name: 'Losses',
-        value: !noRecord ? record.lossCount : '-',
+        value: hasRecord ? record.lossCount : '-',
         inline: false,
       },
       {
         name: 'Win Rate',
-        value: winRate,
+        value: hasRecord ? `${record.winRate}%` : '-',
         inline: false,
       },
       {
         name: 'Streak',
-        value: !noRecord
+        value: hasRecord
           ? record.streakCount == 1
             ? 'No streak'
             : `${record.streakCount} ${record.isWinStreak ? 'wins' : 'losses'}`
